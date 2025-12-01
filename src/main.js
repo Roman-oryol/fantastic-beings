3fc790010b867e95e636cc5490c3235474f08d4f
Roman Oryol <roman.oryol83@gmail.com>
2025-12-01 12:02:42 +0200

    feat: реализовать функцию отрисовки и очистки игрового поля

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
 import './style.css';
+
+window.renderMap = function (rowsCount, colsCount) {
+  const mapElement = document.getElementById('map');
+
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < rowsCount; i++) {
+    const row = document.createElement('tr');
+
+    for (let j = 0; j < colsCount; j++) {
+      const cell = document.createElement('td');
+      cell.classList.add('cell');
+      cell.setAttribute('role', 'gridcell');
+
+      row.appendChild(cell);
+    }
+
+    fragment.appendChild(row);
+  }
+
+  mapElement.appendChild(fragment);
+};
+
+window.clearMap = function () {
+  const mapElement = document.getElementById('map');
+  mapElement.innerHTML = '';
+};
+
+window.renderMap(5, 5);
