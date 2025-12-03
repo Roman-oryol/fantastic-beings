fb8cb2b93686c210a4270813b17fc0a4e02d5e48
Roman Oryol <roman.oryol83@gmail.com>
2025-12-03 08:30:44 +0200

    feat: добавить изображения существ и функции для их отображения на игровом поле

--- src/main.js
+++ src/main.js
@@ -1,4 +1,54 @@
+import zouwu from './images/beings/zouwu.png';
+import swooping from './images/beings/swooping.png';
+import salamander from './images/beings/salamander.png';
+import puffskein from './images/beings/puffskein.png';
+import kelpie from './images/beings/kelpie.png';
+
 import './style.css';
+
+const creatures = [
+  { name: 'zouwu', image: zouwu },
+  { name: 'swooping', image: swooping },
+  { name: 'salamander', image: salamander },
+  { name: 'puffskein', image: puffskein },
+  { name: 'kelpie', image: kelpie },
+];
+
+function getRandomBeing(creatures) {
+  const randomIndex = Math.floor(Math.random() * creatures.length);
+
+  return creatures[randomIndex];
+}
+
+function findBeing(creatures, name) {
+  return creatures.find((obj) => obj.name === name);
+}
+
+function renderBeingInCell(beingObj, cellEl, cellIndex, rowIndex) {
+  const { name, image } = beingObj;
+  const img = document.createElement('img');
+
+  cellEl.setAttribute('data-being', name);
+  img.src = image;
+  img.setAttribute('data-coords', `x${cellIndex}_y${rowIndex}`);
+  cellEl.appendChild(img);
+}
+
+window.fillMapRandomly = function () {
+  const rows = document.querySelectorAll('.row');
+
+  rows.forEach((row, rowIndex) => {
+    const cells = row.querySelectorAll('.cell');
+
+    cells.forEach((cell, cellIndex) => {
+      if (cell.querySelector('img')) {
+        return;
+      }
+
+      renderBeingInCell(getRandomBeing(creatures), cell, cellIndex, rowIndex);
+    });
+  });
+};
 
 window.renderMap = function (rowsCount, colsCount) {
   const mapElement = document.getElementById('map');
@@ -7,11 +57,11 @@ window.renderMap = function (rowsCount, colsCount) {
 
   for (let i = 0; i < rowsCount; i++) {
     const row = document.createElement('tr');
+    row.classList.add('row');
 
     for (let j = 0; j < colsCount; j++) {
       const cell = document.createElement('td');
       cell.classList.add('cell');
-      cell.setAttribute('role', 'gridcell');
 
       row.appendChild(cell);
     }
@@ -27,4 +77,39 @@ window.clearMap = function () {
   mapElement.innerHTML = '';
 };
 
+window.redrawMap = function (mapData) {
+  const rows = mapData.length;
+  const cols = mapData[0].length;
+  const isEqualRows = mapData.every((row) => row.length === cols);
+
+  if (rows < 3 || cols < 3 || !isEqualRows) {
+    return false;
+  }
+
+  window.clearMap();
+  window.renderMap(rows, cols);
+
+  const tableRows = document.querySelectorAll('.row');
+
+  tableRows.forEach((row, rowIndex) => {
+    const cells = row.querySelectorAll('.cell');
+
+    cells.forEach((cell, cellIndex) => {
+      const beingName = mapData[rowIndex][cellIndex];
+      const being = findBeing(creatures, beingName);
+
+      renderBeingInCell(being, cell, cellIndex, rowIndex);
+    });
+  });
+
+  return true;
+};
+
 window.renderMap(5, 5);
+window.fillMapRandomly();
+// window.redrawMap([
+//   ['kelpie', 'puffskein', 'kelpie', 'puffskein'],
+//   ['swooping', 'zouwu', 'puffskein', 'zouwu'],
+//   ['kelpie', 'puffskein', 'zouwu', 'puffskein'],
+//   ['kelpie', 'puffskein', 'zouwu', 'kelpie'],
+// ]);
